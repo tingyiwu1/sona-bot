838bb4321fc2601cbd6a8a7c0e4aa457e10a3fce
TingYi Wu <tingyi.wu1@gmail.com>
2022-12-09 11:05:21 -0500

    start notif

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from sona import sona
 import asyncio
+from aiohttp import ClientSession
 from dotenv import load_dotenv
 import os
 
@@ -13,38 +14,40 @@ USERNAME, PASSWORD, DISC_URL = (
 
 
 async def main():
-    try:
-        while True:
-            async with sona(USERNAME, PASSWORD) as session:
-                while True:
-                    r = await session.get(
-                        "https://bu.sona-systems.com/all_exp_participant.aspx"
-                    )
-                    if r.url == "https://bu.sona-systems.com/default.aspx":
-                        break  # session expired, retry
-                    # print(await (await session.post("https://bu.sona-systems.com/AjaxMethods.asmx/RenewSession")).text())
-                    if "No studies are available at this time." in await r.text():
-                        print("No studies available :(")
-                    else:
-                        print("Study available!")
-                        await session.post(
-                            DISC_URL,
-                            data={
-                                "content": "Study available!",
-                                "username": "sona-bot",
-                            },
+    async with ClientSession() as session:
+        try:
+            await session.post(DISC_URL, data={"content": "Started", "username": "sona-bot"})
+            while True:
+                async with sona(USERNAME, PASSWORD, session) as session:
+                    while True:
+                        r = await session.get(
+                            "https://bu.sona-systems.com/all_exp_participant.aspx"
                         )
-                    await asyncio.sleep(10)
-    except Exception as e:
-        await session.post(
-            DISC_URL,
-            data={"content": f"Error: {e}", "username": "sona-bot"},
-        )
-    finally:
-        await session.post(
-            DISC_URL,
-            data={"content": "Stopped", "username": "sona-bot"},
-        )
+                        if r.url == "https://bu.sona-systems.com/default.aspx":
+                            break  # session expired, retry
+                        # print(await (await session.post("https://bu.sona-systems.com/AjaxMethods.asmx/RenewSession")).text())
+                        if "No studies are available at this time." in await r.text():
+                            print("No studies available :(")
+                        else:
+                            print("Study available!")
+                            await session.post(
+                                DISC_URL,
+                                data={
+                                    "content": "Study available!",
+                                    "username": "sona-bot",
+                                },
+                            )
+                        await asyncio.sleep(10)
+        except Exception as e:
+            await session.post(
+                DISC_URL,
+                data={"content": f"Error: {e}", "username": "sona-bot"},
+            )
+        finally:
+            await session.post(
+                DISC_URL,
+                data={"content": "Stopped", "username": "sona-bot"},
+            )
 
 
 if __name__ == "__main__":
--- sona.py
+++ sona.py
@@ -4,14 +4,13 @@ import re
 
 
 @asynccontextmanager
-async def sona(username: str, password: str):
-    async with aiohttp.ClientSession() as session:
-        r = await session.get("https://bu.sona-systems.com/default.aspx")
-        matches = re.findall(r'<input type="hidden" name="(.+)" id.+value="(.+)"', await r.text())
-        data = dict(matches) | {
-            "ctl00$ContentPlaceHolder1$userid": username,
-            "ctl00$ContentPlaceHolder1$pw": password,
-            "ctl00$ContentPlaceHolder1$default_auth_button": "Log In",
-        }
-        await session.post("https://bu.sona-systems.com/default.aspx", data=data)
-        yield session
+async def sona(username: str, password: str, session: aiohttp.ClientSession):
+    r = await session.get("https://bu.sona-systems.com/default.aspx")
+    matches = re.findall(r'<input type="hidden" name="(.+)" id.+value="(.+)"', await r.text())
+    data = dict(matches) | {
+        "ctl00$ContentPlaceHolder1$userid": username,
+        "ctl00$ContentPlaceHolder1$pw": password,
+        "ctl00$ContentPlaceHolder1$default_auth_button": "Log In",
+    }
+    await session.post("https://bu.sona-systems.com/default.aspx", data=data)
+    yield session
