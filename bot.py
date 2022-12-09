0ce627050ad9a2cae6e884f11de26d2d297991a4
TingYi Wu <tingyi.wu1@gmail.com>
2022-12-09 10:57:23 -0500

    bot

---
+++ bot.py
@@ -0,0 +1,53 @@
+from sona import sona
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+USERNAME, PASSWORD, DISC_URL = (
+    os.getenv("USERNAME"),
+    os.getenv("PASSWORD"),
+    os.getenv("DISC_URL"),
+)
+
+
+async def main():
+    try:
+        while True:
+            async with sona(USERNAME, PASSWORD) as session:
+                logged_in = True
+                while logged_in:
+                    r = await session.get(
+                        "https://bu.sona-systems.com/all_exp_participant.aspx"
+                    )
+                    if r.url == "https://bu.sona-systems.com/default.aspx":
+                        logged_in = False
+                        break
+                    # print(await (await session.post("https://bu.sona-systems.com/AjaxMethods.asmx/RenewSession")).text())
+                    if "No studies are available at this time." in await r.text():
+                        print("No studies available :(")
+                    else:
+                        print("Study available!")
+                        await session.post(
+                            DISC_URL,
+                            data={
+                                "content": "Study available!",
+                                "username": "sona-bot",
+                            },
+                        )
+                    await asyncio.sleep(10)
+    except Exception as e:
+        await session.post(
+            DISC_URL,
+            data={"content": f"Error: {e}", "username": "sona-bot"},
+        )
+    finally:
+        await session.post(
+            DISC_URL,
+            data={"content": "Stopped", "username": "sona-bot"},
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
